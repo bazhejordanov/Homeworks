2129e5770b3264d277a1add2ac5ea1e9b5596633
bazhejordanov <150715115+bazhejordanov@users.noreply.github.com>
2023-12-04 17:09:39 +0100

    Add files via upload

---
+++ JavaScript-price calculation/script.js
@@ -0,0 +1,25 @@
+console.log('PHONE CALCULATING')
+let numberOfPhones=30
+let priceOfPhone=119.95
+let taxRate=5
+console.log('calculating tax')
+console.log(priceOfPhone*taxRate/100)
+
+let priceOfOnePhone=125.94
+console.log('price for one phone with tax')
+console.log(125.94)
+console.log('price for 30 phones with tax')
+console.log(numberOfPhones*priceOfOnePhone)
+//Dodadov mozebi poveke consol.log (mozese i bez niv) za objasnuvanje nad iznosite za da bide porazbirlivo koj iznos za sto e
+
+//od cas
+console.log('FEET TO METERS')
+let feet=21
+let meters=0.3048
+console.log(feet*meters)
+
+//od cas
+console.log('TWO SIDES OF RECTANGLE')
+let sideA=7
+let sideB=5
+console.log(sideA*sideB)
