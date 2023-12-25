9866fc471f5599ce072b1abd194863e05d30688d
bazhejordanov <150715115+bazhejordanov@users.noreply.github.com>
2023-12-25 14:11:53 +0100

    Add files via upload

---
+++ Change document,print numbers from an array and the sum/script.js
@@ -0,0 +1,10 @@
+let h1=document.getElementById("myTitle")
+h1.innerText="My changed page"
+
+let paragraph1=document.getElementsByClassName("paragraph")[0]
+paragraph1.innerText="This is a homework"
+
+let paragraph2=document.getElementsByClassName("second")[0]
+paragraph2.innerText="Alright"
+
+//nemozev poslednite dve da gi smenam
