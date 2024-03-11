dc9bed5320d23e5dbe221a5089f8f80366c66009
bazhejordanov <150715115+bazhejordanov@users.noreply.github.com>
2024-03-11 13:37:21 +0100

    Add files via upload

---
+++ HW4 task1.cs
@@ -0,0 +1,16 @@
+﻿//
+string inputName = Console.ReadLine();
+if  (inputName.Length >= 5)
+{
+    string name=inputName.Substring(inputName.Length - 5);
+    Console.WriteLine(name);
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
+//
+string surName = "Jordanov";
+string surName2 =surName.Substring(3);
+Console.WriteLine(surName2);
