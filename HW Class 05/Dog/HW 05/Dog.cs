ee2685cff2e47d1ae85e01958dc1a65737147cdc
bazhejordanov <150715115+bazhejordanov@users.noreply.github.com>
2024-03-18 15:42:17 +0100

    Add files via upload

---
+++ HW Class 05/Dog/HW 05/Dog.cs
@@ -0,0 +1,29 @@
+﻿namespace HW_05
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+
+        public Dog(string name, string breed, string color)
+        {
+            Name = name;
+            Breed = breed;
+            Color = color;
+        }
+
+        public string Eat()
+        {
+            return "The dog is now eating";
+        }
+        public string Play()
+        {
+            return "The dog is now playing";
+        }
+        public string ChaseTail()
+        {
+            return "The dog is now chasing it's tail";
+        }
+    }
+}
---
+++ HW Class 05/Dog/HW 05/Program.cs
@@ -0,0 +1,35 @@
+﻿using HW_05;
+
+Console.WriteLine("Enter dog's name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter dog's breed:");
+string breed = Console.ReadLine();
+
+Console.WriteLine("Enter dog's color:");
+string color = Console.ReadLine();
+
+Dog dog=new Dog(name, breed, color);
+
+Console.WriteLine("Choose: ");
+Console.WriteLine("1. Eat");
+Console.WriteLine("2. Play");
+Console.WriteLine("3. Chase Tail");
+
+int choose=int.Parse(Console.ReadLine());
+
+switch (choose)
+{
+    case 1:
+        Console.WriteLine(dog.Eat());
+        break;
+    case 2:
+        Console.WriteLine(dog.Play());
+        break;
+    case 3:
+        Console.WriteLine(dog.ChaseTail());
+        break;
+    default:
+        Console.WriteLine("Error");
+        break;
+}
